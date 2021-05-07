fd87013331e8af20128b72207064de71a7dd9978
Kateti Mareko <katetimareko@codenertapps.com>
2021-05-08 02:47:19 +1200

    Add not knex db not null constraints

--- db/migrations/20210507000315_stores.js
+++ db/migrations/20210507000315_stores.js
@@ -2,12 +2,12 @@
 exports.up = (knex) => {
 	return knex.schema.createTable('stores', table => {
 		table.increments('id').primary()
-		table.string('store_name')
-		table.string('admin_first_name')
-		table.string('admin_last_name')
-		table.string('email')
-		table.integer('phone')
-		table.string('address')
+		table.string('store_name').notNullable()
+		table.string('admin_first_name').notNullable()
+		table.string('admin_last_name').notNullable()
+		table.string('email').notNullable()
+		table.integer('phone').notNullable()
+		table.string('address').notNullable()
 	})
 }
   
--- db/migrations/20210507003252_store_users.js
+++ db/migrations/20210507003252_store_users.js
@@ -2,9 +2,9 @@
 exports.up = (knex) => {
 	return knex.schema.createTable('store_users', table => {
 		table.increments('id').primary()
-		table.integer('store_id').references('stores.id')
-		table.string('user_id')
-		table.integer('stamp_count')
+		table.integer('store_id').references('stores.id').notNullable()
+		table.string('user_id').notNullable()
+		table.integer('stamp_count').notNullable()
 	})
 }
   
